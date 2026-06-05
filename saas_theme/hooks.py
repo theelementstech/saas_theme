90d6def4a97ad551f39b59365f5e2e0bb056fefc
05elements <theelementstech@gmail.com>
2026-06-05 17:34:51 +0000

    fix: multiselect field issue

--- saas_theme/hooks.py
+++ saas_theme/hooks.py
@@ -27,8 +27,8 @@ categories = ["Customization", "UI/UX"]
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/saas_theme/css/saas_theme.css?v=59"
-app_include_js = "/assets/saas_theme/js/saas_theme.js?v=59"
+app_include_css = "/assets/saas_theme/css/saas_theme.css?v=65"
+app_include_js = "/assets/saas_theme/js/saas_theme.js?v=65"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/saas_theme/css/login.css"
